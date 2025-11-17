bd7dca2256ba52dc8a7f4bcffdda0fba8d1adc86
wailhassan <wail.alammari@gmail.com>
2025-11-17 10:47:22 +0000

    Adding new features using localhost request resources

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 const app = express();
-
 const port = 3000;
+
+// To get response form the server
+app.get("/", (req, res) => {
+  res.send("Hello Wail, I'm here for you");
+});
 
 app.listen(port, () => {
   console.log(`Server Running on port ${port}.`);
